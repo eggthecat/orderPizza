346d5f29559af2ca4a5e486b3ce628b4425684d4
Dominic Montelongo <montelongodominic@email.com>
2019-06-21 08:43:13 -0700

    Add constuctor for pizza(incomplete)

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// business logic
+
+let Pizza {
+  var size = ["Mini","Personal","Medium","Large"],
+  var customDough = ["Gluten-Free","Vegetarian","Vegan","Gundry"],
+  var sauce = ["Red","White","Pesto","Gluten-Free","Vegetarian","Vegan","Gundry"],
+  var vegtebleToppings =, "Alfalfa Sprouts", "Artichoke hearts", "Avocado", "Baby leeks",     "Beetroot", "Black Beans", "Broccoli", "Capers", "Capicolla", "Carrot", "Cherry tomatoes", "Dried tomatoes", "Eggplant", "Fungi", "Fungi carciofi", "Green peppers", "Kalamata olives", "Lettuce", "Mushrooms", "Onions, "Olives", "Peas", "Porcini mushrooms", "Portobello Mushrooms", "Red beans", "Red onions", "Red peppers", "Roast cauliflower", "Roasted eggplant", "Roasted Garlic", "Roasted peppers", "scallions", "Shallots", "Snow peas", "Spinach", "Sun dried tomatoes", "Sweet corn", "Watercress", "Wild mushrooms", "Yellow peppers", "Yellow squash", "Zucchini"],
+  var nutToppings = ["Almonds", "Peanuts", "Pistachios", "Pecans", "Pine Nuts", "Walnuts"],
+  var herbToppings = ["Basil", "Bay Leaf", "Cardamon", "Chili (Dried or Fresh)", "Chives", "Cilantro", "Coriander", "Cumin", "Dill", "Garlic", "Jalapeno Peppers", "Laurel", "Marjoram", "Methi Leaves (aka Fenugreek)", "Oregano", "Parley", "Pepper", "Rosemary"],
+  var seaFood =
+
+}
