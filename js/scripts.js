b47d1fa3baa84df6dba8bf4f116605e20eb79d72
Dominic Montelongo <montelongodominic@email.com>
2019-06-21 14:20:58 -0700

    Add input for address and experirment with var in js

--- js/scripts.js
+++ js/scripts.js
@@ -18,29 +18,29 @@ $(document).ready(function(){
     // var Input = $(input#).val();
     // var flavor = $("input:radio[name=flavor]:checked").val();
     var customerInput = $(input#name).val();
-    // true or false delivery option
-    var deliveryInput = $(input#delivery).val();
-    // true of false pick-up option
-    var pickInput = $(input#pick).val();
-    // option to schedule pick up of pizza
-    //**MAKE SURE TO CREATE ALERT IF BOTH ARE UNSELECTED OR MATCH SELECTED VALUES LIKE YES&&YES
-    var timeInput = $(input#time).val();
-    // input field for customer delivery address
-    var addressInput = $(input#address).val();
-    // input field for customer delivery address
-    var cityInput = $(input#city).val();
-    // input field for customer phone number
-    var phoneInput = $(input#phone).val();
+    // // true or false delivery option
+    // var deliveryInput = $(input#delivery).val();
+    // // true of false pick-up option
+    // var pickUpInput = $(input#pick).val();
+    // // option to schedule pick up of pizza
+    // //**MAKE SURE TO CREATE ALERT IF BOTH ARE UNSELECTED OR MATCH SELECTED VALUES LIKE YES&&YES
+    // var timeInput = $(input#time).val();
+    // // input field for customer delivery address
+    // var addressInput = $(input#address).val();
+    // // input field for customer delivery address
+    // var cityInput = $(input#city).val();
+    // // input field for customer phone number
+    // var phoneInput = $(input#phone).val();
     // input field for pizza size
-    var sizeInput = $(input#size).val();
-    var doughInput = $(input#dough).val();
-    var sauceInput = $(input#sauce).val();
-    var vegtebleToppingsInput = $(input#vegtebleToppings).val();
-    var nutToppingsInput = $(input#nutToppings).val();
-    var herbToppings = $(herbToppings#size).val();
-    var seafoodInput = $(input#seafood).val();
-    var meatInput = $(input#meat).val();
-    var cheeseInput = $(input#cheese).val();
-    var deliveryInput = $(input#delivery).val();
-  })
+  //   var sizeInput = $(input#size).val();
+  //   var doughInput = $(input#dough).val();
+  //   var sauceInput = $(input#sauce).val();
+  //   var vegtebleToppingsInput = $(input#vegtebleToppings).val();
+  //   var nutToppingsInput = $(input#nutToppings).val();
+  //   var herbToppings = $(herbToppings#size).val();
+  //   var seafoodInput = $(input#seafood).val();
+  //   var meatInput = $(input#meat).val();
+  //   var cheeseInput = $(input#cheese).val();
+  //   var deliveryInput = $(input#delivery).val();
+  // })
 })
