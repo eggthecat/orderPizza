d5c4bfb7e43239b0143b04e7c8046f59361b0f7a
Dominic Montelongo <montelongodominic@email.com>
2019-06-23 22:37:11 -0700

    Add pizza constructor to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+//business logic
+
+function Pizza = (name, phone, address, city, size, dough, sauce,cheese, topping) {
+  this.name = name;
+  this.phone = colors;
+  this.address = address,
+  this.city = city,
+  this.size = size,
+  this.dough = dough,
+  this.sauce = sauce,
+  this.cheese = cheese,
+  this.topping = topping,
+}
+
+
+
+
+
+// front-end logic
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    // var Input = $(input#).val();
+    // var flavor = $("input:radio[name=flavor]:checked").val();
+    // var customerInput = $(input#name).val([]);
+    // // // true or false delivery option
+    // var deliveryInput = $(input#delivery).val();
+    // // // true of false pick-up option
+    // var pickUpInput = $(input#pick).val();
+    // // option to schedule pick up of pizza
+    // // **MAKE SURE TO CREATE ALERT IF BOTH ARE UNSELECTED OR MATCH SELECTED VALUES LIKE YES&&YES//
+    // var timeInput = $(input#time).val();
+    // // // input field for customer delivery address
+    // var addressInput = $(input#address).val();
+    // // // input field for customer delivery address
+    // var cityInput = $(input#city).val();
+    // // // input field for customer phone number
+    // var phoneInput = $(input#phone).val();
+    // console.log(#name);
+    // console.log(#city);
+    // console.log(phone);
+    // input field for pizza size
+    // var sizeInput = $(input#size).val();
+    // var doughInput = $(input#dough).val();
+    // var sauceInput = $(input#sauce).val();
+    // var vegtebleToppingsInput = $(input#vegtebleToppings).val();
+    // var nutToppingsInput = $(input#nutToppings).val();
+    // var herbToppings = $(herbToppings#size).val();
+    // var seafoodInput = $(input#seafood).val();
+    // var meatInput = $(input#meat).val();
+    // var cheeseInput = $(input#cheese).val();
+    // var deliveryInput = $(input#delivery).val();
+  })
+})
